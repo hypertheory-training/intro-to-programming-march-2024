660e89de42643d4e700986d5707477839b3ad41f
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-05 16:39:38 -0500

    done

--- src/week1/BankingSolution/Banking.UnitTests/Accounts/MakingWithdrawls.cs
+++ src/week1/BankingSolution/Banking.UnitTests/Accounts/MakingWithdrawls.cs
@@ -17,4 +17,25 @@ public class MakingWithdrawls
 
         Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
     }
+
+    [Fact]
+    public void OverdraftNotAllowed()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        account.Withdraw(account.GetBalance() + .01M);
+
+        Assert.Equal(openingBalance, account.GetBalance());
+    }
+
+    [Fact]
+    public void AreAnyDecimalsAllowed()
+    {
+        var account = new BankAccount();
+
+        account.Withdraw(-5230);
+
+        Assert.Equal(5000M, account.GetBalance());
+    }
 }
