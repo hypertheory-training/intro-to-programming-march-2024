425c92bad95e1f57632d48f1b3cc6a1d51e72027
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-05 16:31:53 -0500

    done for the day

---
+++ src/week1/BankingSolution/Banking.UnitTests/Accounts/MakingDeposits.cs
@@ -0,0 +1,25 @@
+﻿
+using Bank;
+
+namespace Banking.UnitTests.Accounts;
+public class MakingDeposits
+{
+    [Theory]
+    [InlineData(100)]
+    [InlineData(42.23)]
+    public void MakingADespositIncreasesBalance(decimal amountToDeposit)
+    {
+        // Given
+        var account = new BankAccount();
+        // Get Balance is a "Query" - we are asking it for something.
+        var openingBalance = account.GetBalance();
+
+        // When
+        // WTCYWYH 
+        // Command - Telling it to do some work.
+        account.Deposit(amountToDeposit);
+
+        // Then
+        Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+    }
+}
---
+++ src/week1/BankingSolution/Banking.UnitTests/Accounts/MakingWithdrawls.cs
@@ -0,0 +1,20 @@
+﻿
+using Bank;
+
+namespace Banking.UnitTests.Accounts;
+public class MakingWithdrawls
+{
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(2.25)]
+    public void MakingAWithdrawalDecreasesTheBalance(decimal amountToWithdraw)
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+
+        account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
+    }
+}
