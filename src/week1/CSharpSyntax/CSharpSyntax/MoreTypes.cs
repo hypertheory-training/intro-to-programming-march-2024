c53a04c7d7a3478595924dd2149d1b03c7bdc6ad
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-08 15:10:02 -0500

    half way!

--- src/week1/CSharpSyntax/CSharpSyntax/MoreTypes.cs
+++ src/week1/CSharpSyntax/CSharpSyntax/MoreTypes.cs
@@ -8,4 +8,43 @@ public class MoreTypes
     // 4. Constructors
     // 5. More about "kinds" of objects - records
 
+    [Fact]
+    public void PropertiesAndStuff()
+    {
+        var c = new Customer();
+
+        c.Name = "Bob";
+
+        Assert.Equal("Bob", c.Name);
+
+        Object someObj;
+
+        someObj = c;
+
+
+        someObj = 23;
+
+        //var myLuckyNumbers = new ArrayList()
+        //{
+        //    c,10, 20, 108
+        //};
+        var myLuckyNumbers = new List<int>()
+        {
+           1,10, 20, 108
+        };
+
+        var myCustomers = new Dictionary<string, Customer>() {
+            { "bob", new Customer() { Name="Bob Smith"} },
+            { "sue", new Customer() { Name="Sue Jones"} }
+        };
+
+        Assert.Equal(0, myCustomers["sue"].CreditLimit);
+
+
+        var sum = myLuckyNumbers[0] + myLuckyNumbers[1];
+
+        Assert.Equal(11, sum);
+
+
+    }
 }
