4fbc95a1ad256b750850c44a809a0f19eb4213c9
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-08 13:29:17 -0500

    planning

---
+++ src/week1/CSharpSyntax/CSharpSyntax/MoreTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpSyntax;
+public class MoreTypes
+{
+    // Todo After Lunch
+    // 1. "First Class Citizens"
+    // 2. Delegates
+    // 3. Properties
+    // 4. Constructors
+    // 5. More about "kinds" of objects - records
+
+}
