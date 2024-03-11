10e5ef25c914bb58db412d7321764c319d38a0a1
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-11 14:50:21 -0400

    before break

---
+++ src/week2/PropertiesThing/PropertiesThing/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var account = new BankAccount();
+
+
+account.Deposit(100M);
+account.Deposit(200M);
+
+foreach (var h in account.GetDepositHistory())
+{
+
+
+    Console.WriteLine($"On {h.DepositedOn} you deposited {h.Amount:c}");
+}
+
+
+
+
+var s1 = new Student { FirstName = "Joe", LastName = "Schmidt" };
+var s2 = new Student { FirstName = "Joe", LastName = "Schmidt" };
+
+Console.WriteLine(s1.ToString());
+var areTheSame = s1 == s2;
+
+//var newJoe = new Student { FirstName = "Joseph", LastName = s1.LastName };
+var newJoe = s1 with { FirstName = "Joseph" };
+Console.WriteLine(newJoe);
+Console.WriteLine("They are the same? " + areTheSame);
+
+public class BankAccount
+{
+    private List<DepositRecord> depositRecords = new();
+
+    public int Id { get; set; }
+
+    // properties:
+    // should not throw exceptions.
+    // should not involve "compute" - like there is no Async stuff for this.
+    // should return the same value for every "get" after it is set.
+
+    public string FirstName { get; set; } = string.Empty;
+    public decimal Balance { get; private set; } = 5000;
+
+    public void Deposit(decimal amount)
+    {
+        var dr = new DepositRecord(amount, DateTime.Now);
+
+        depositRecords.Add(dr);
+        Balance += amount;
+    }
+
+    public IEnumerable<DepositRecord> GetDepositHistory()
+    {
+        return depositRecords;
+    }
+}
+
+public record DepositRecord(decimal Amount, DateTime DepositedOn);
+
+public record Student
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -1,22 +1,15 @@
 import { Component } from '@angular/core';
-import { PageHeaderComponent } from "./components/page-header/page-header.component";
-
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    template: `
-   <app-page-header />
-   <main  >
-    
-    <p>Our Stuff Goes Here</p>
-  <button class="btn btn-primary">Click Me For A Good Time</button>
-
-   </main>
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <app-page-header />
+    <main></main>
   `,
-    styles: [],
-    imports: [PageHeaderComponent]
+  styles: [],
+  imports: [PageHeaderComponent, DemoComponent],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
