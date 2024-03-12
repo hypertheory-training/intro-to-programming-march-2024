44745cc6611ce23de6864f2183a4ad5a5f9880e1
Jeffry Gonzalez <jeff@hypertheory.com>
2024-03-12 14:36:29 -0400

    can count

---
+++ src/week2/app/frontend/src/app/pages/counter/state/actions.ts
@@ -0,0 +1,9 @@
+import { createActionGroup, emptyProps } from '@ngrx/store';
+
+export const CounterAction = createActionGroup({
+  source: 'Counter Component Events',
+  events: {
+    'Incremented The Count': emptyProps(),
+    'Decremented The Count': emptyProps(),
+  },
+});
--- src/week2/app/frontend/src/app/pages/counter/state/index.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/index.ts
@@ -1,1 +1,23 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { CounterAction } from './actions';
 
+export interface CounterState {
+  current: number;
+}
+
+const initialState: CounterState = {
+  current: 0,
+};
+
+export const counterFeature = createFeature({
+  name: 'counter',
+  reducer: createReducer(
+    initialState,
+    on(CounterAction.incrementedTheCount, (state) => ({
+      current: state.current + 1,
+    })),
+    on(CounterAction.decrementedTheCount, (state) => ({
+      current: state.current - 1,
+    }))
+  ),
+});
